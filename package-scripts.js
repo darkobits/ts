4570c24a30b15bdb0a86e9c0d608295a27bf2284
darkobits <joshua+github@frontlawn.net>
2020-11-17 00:57:45 -0800

    chore: Add publish script.

--- package-scripts.js
+++ package-scripts.js
@@ -3,8 +3,8 @@ require('./src/etc/babel-register');
 module.exports = require('./src/config/package-scripts')(({ npsUtils }) => ({
   scripts: {
     publish: npsUtils.series(
-      'nps build',
-      './dist/scripts/re-pack.js'
+      'npx re-pack',
+      'npm publish .re-pack --tag=beta'
     )
   }
 }));
