4a6288255f8e0b27fb5b9fd1846a56747d22817e
darkobits <joshua+github@frontlawn.net>
2020-11-16 21:51:42 -0800

    feat: Add re-pack.

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,5 +3,8 @@ require('./src/etc/babel-register');
 module.exports = {
   extends: [
     './src/config/eslint'
-  ]
+  ],
+  rules: {
+    'unicorn/no-reduce': 'off'
+  }
 }
--- package-scripts.js
+++ package-scripts.js
@@ -1,3 +1,10 @@
 require('./src/etc/babel-register');
 
-module.exports = require('./src/config/package-scripts')();
+module.exports = require('./src/config/package-scripts')(({ npsUtils }) => ({
+  scripts: {
+    publish: npsUtils.series(
+      'nps build',
+      './dist/scripts/re-pack.js'
+    )
+  }
+}));
