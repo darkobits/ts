1a86d6544cb10e5c3531a00ba2a843a77bad3c7d
darkobits <joshua+github@frontlawn.net>
2020-11-19 22:16:47 -0800

    chore: Update package scripts.

--- package-scripts.js
+++ package-scripts.js
@@ -4,7 +4,7 @@ module.exports = require('./src/config/package-scripts')(({ npsUtils }) => ({
   scripts: {
     publish: npsUtils.series(
       'npx re-pack',
-      'npm publish .re-pack --tag=beta'
+      'npm publish .re-pack --tag=beta --ignore-scripts'
     )
   }
 }));
