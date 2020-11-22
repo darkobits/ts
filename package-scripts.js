d7886d22557c5aab131cc856fd8a1921a6c72e2f
darkobits <joshua+github@frontlawn.net>
2020-11-21 22:41:25 -0800

    chore: Update dependencies.

--- package-scripts.js
+++ package-scripts.js
@@ -2,7 +2,7 @@ require('./src/etc/babel-register');
 
 module.exports = require('./src/config/package-scripts')(({ npsUtils }) => ({
   scripts: {
-    publish: 're-pack --publish',
+    publish: 're-pack publish',
     repack: {
       script: 're-pack',
       watch: 're-pack --link --watch'
