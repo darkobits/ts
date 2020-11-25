868b5f21237eb506789a8fcb21cdde935041d253
darkobits <joshua+github@frontlawn.net>
2020-11-24 19:33:23 -0800

    chore: Update Babel config.

--- src/config/babel.js
+++ src/config/babel.js
@@ -1,11 +1,12 @@
 module.exports = {
   extends: require('@darkobits/ts').babel,
   presets: [
-    ['@babel/preset-env', {
+    [require.resolve('@babel/preset-env'), {
       useBuiltIns: 'entry',
       corejs: 3
     }],
-    require.resolve('linaria/babel')
+    require.resolve('linaria/babel'),
+    require.resolve('@babel/preset-react')
   ],
   plugins: [
     require.resolve('react-hot-loader/babel')
