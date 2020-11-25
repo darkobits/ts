b2a6057bf776dd02c74f40d7321082b3adee0f11
darkobits <joshua+github@frontlawn.net>
2020-11-24 19:16:32 -0800

    refactor: Update various utility functions.

--- src/lib/utils.spec.ts
+++ src/lib/utils.spec.ts
@@ -1,6 +1,9 @@
 import readPkgUp from 'read-pkg-up';
 import resolvePkg from 'resolve-pkg';
-import {requireBin} from 'lib/utils';
+import {
+  requireBin,
+  getUserScripts
+} from 'lib/utils';
 
 
 jest.mock('resolve-pkg', () => {
@@ -17,35 +20,42 @@ jest.mock('resolve-pkg', () => {
       return false;
     }
 
-    throw new Error(`Unknown package name: ${pkgName}`);
+    throw new Error(`[mock] Unknown package name: ${pkgName}`);
   });
 });
 
-jest.mock('read-pkg-up', () => jest.fn(async ({cwd}) => {
-  if (cwd === '/path/to/pkg-with-named-bin') {
-    return {
-      packageJson: {
-        bin: {
-          'pkg-with-named-bin': '/path/to/pkg-with-named-bin/bin'
-        }
-      },
-      path: '/path/to/pkg-with-named-bin'
-    };
-  }
 
-  if (cwd === '/path/to/pkg-with-no-bin') {
-    return {
-      packageJson: {},
-      path: '/path/to/pkg-with-no-bin'
-    };
-  }
+jest.mock('read-pkg-up', () => {
+  const sync = jest.fn(({ cwd }) => {
+    if (cwd === '/path/to/pkg-with-named-bin') {
+      return {
+        packageJson: {
+          bin: {
+            'pkg-with-named-bin': '/path/to/pkg-with-named-bin/bin'
+          }
+        },
+        path: '/path/to/pkg-with-named-bin'
+      };
+    }
 
-  if (cwd === '/path/to/non-existent-pkg') {
-    return false;
-  }
+    if (cwd === '/path/to/pkg-with-no-bin') {
+      return {
+        packageJson: {},
+        path: '/path/to/pkg-with-no-bin'
+      };
+    }
 
-  throw new Error(`Unknown cwd: ${cwd}`);
-}));
+    if (cwd === '/path/to/non-existent-pkg') {
+      return false;
+    }
+
+    throw new Error(`Unknown cwd: ${cwd}`);
+  });
+
+  return {
+    sync
+  };
+});
 
 
 describe('requireBin', () => {
@@ -54,33 +64,33 @@ describe('requireBin', () => {
   });
 
   describe('when the package defines binaries', () => {
-    it('should atempt to require the indicated binary', async () => {
+    it('should attempt to require the indicated binary', () => {
       expect.assertions(3);
 
       try {
-        await requireBin('pkg-with-named-bin');
+        requireBin('pkg-with-named-bin');
       } catch (err) {
         expect(err.message).toMatch('Cannot find module');
-        // @ts-ignore
+        // @ts-expect-error
         expect(resolvePkg.mock.calls[0][0]).toBe('pkg-with-named-bin');
-        // @ts-ignore
-        expect(readPkgUp.mock.calls[0][0]).toMatchObject({cwd: '/path/to/pkg-with-named-bin'});
+        // @ts-expect-error
+        expect(readPkgUp.sync.mock.calls[0][0]).toMatchObject({cwd: '/path/to/pkg-with-named-bin'});
       }
     });
   });
 
   describe('when the package does not define binaries', () => {
-    it('should throw an error', async () => {
+    it('should throw an error', () => {
       expect.assertions(3);
 
       try {
-        await requireBin('pkg-with-no-bin');
+        requireBin('pkg-with-no-bin');
       } catch (err) {
-        expect(err.message).toMatch('Package "pkg-with-no-bin" does not declare any binaries.');
-        // @ts-ignore
+        expect(err.message).toMatch('"pkg-with-no-bin" does not export any binaries');
+        // @ts-expect-error
         expect(resolvePkg.mock.calls[0][0]).toBe('pkg-with-no-bin');
-        // @ts-ignore
-        expect(readPkgUp.mock.calls[0][0]).toMatchObject({cwd: '/path/to/pkg-with-no-bin'});
+        // @ts-expect-error
+        expect(readPkgUp.sync.mock.calls[0][0]).toMatchObject({cwd: '/path/to/pkg-with-no-bin'});
       }
     });
   });
@@ -90,29 +100,54 @@ describe('requireBin', () => {
       expect.assertions(3);
 
       try {
-        await requireBin('non-existent-pkg');
+        requireBin('non-existent-pkg');
       } catch (err) {
-        expect(err.message).toMatch('Unable to resolve path to package "non-existent-pkg".');
-        // @ts-ignore
+        expect(err.message).toMatch('Unable to resolve path to package "non-existent-pkg"');
+        // @ts-expect-error
         expect(resolvePkg.mock.calls[0][0]).toBe('non-existent-pkg');
-        expect(readPkgUp).not.toHaveBeenCalled();
+        // eslint-disable-next-line @typescript-eslint/unbound-method
+        expect(readPkgUp.sync).not.toHaveBeenCalled();
       }
     });
   });
 
   describe('when the indicated binary does not exist', () => {
-    it('should throw an error', async () => {
+    it('should throw an error', () => {
       expect.assertions(3);
 
       try {
-        await requireBin('pkg-with-named-bin', 'bad-bin-name');
+        requireBin('pkg-with-named-bin', 'bad-bin-name');
       } catch (err) {
-        expect(err.message).toMatch('Package "pkg-with-named-bin" does not have binary "bad-bin-name".');
-        // @ts-ignore
+        expect(err.message).toMatch('"pkg-with-named-bin" does not export a binary named "bad-bin-name"');
+        // @ts-expect-error
         expect(resolvePkg.mock.calls[0][0]).toBe('pkg-with-named-bin');
-        // @ts-ignore
-        expect(readPkgUp.mock.calls[0][0]).toMatchObject({cwd: '/path/to/pkg-with-named-bin'});
+        // @ts-expect-error
+        expect(readPkgUp.sync.mock.calls[0][0]).toMatchObject({cwd: '/path/to/pkg-with-named-bin'});
       }
+    });
+  });
+});
+
+
+describe('getUserScripts', () => {
+  const userScripts = {scripts: {}};
+
+  describe('when provided a function', () => {
+    it('should invoke the function and return the result', () => {
+      const configFactory = () => userScripts;
+      expect(getUserScripts(configFactory)).toEqual(userScripts);
+    });
+  });
+
+  describe('when provided an object', () => {
+    it('should return the object', () => {
+      expect(getUserScripts(userScripts)).toEqual(userScripts);
+    });
+  });
+
+  describe('when provided no argument', () => {
+    it('should return a configuration scaffold', () => {
+      expect(getUserScripts()).toMatchObject({scripts: {}});
     });
   });
 });
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -10,42 +10,62 @@ import log from 'lib/log';
 
 
 /**
- * Resolves the absolute path to the binary of a given package.
+ * Synchronously searches for a directory containing a package.json starting
+ * from process.cwd() or the provided directory. Returns an object containing
+ * a normalized package.json manifest and the root directory of the project.
  */
-export async function resolveBin(pkgName: string, binName?: string) {
-  // Resolve the indicated package relative to this package.
-  const pkgPath = resolvePkg(pkgName, {cwd: __dirname});
+export function getPackageInfo(cwd?: string) {
+  const pkgInfo = readPkgUp.sync({ cwd });
 
-  if (!pkgPath) {
-    throw new Error(`[resolveBin] Unable to resolve path to package "${pkgName}".`);
+  if (!pkgInfo) {
+    throw new Error(`${log.prefix('getPackageInfo')} Unable to find a package.json for the project.`);
   }
 
-  // Load the package.json for the indicated package.
-  const pkgData = await readPkgUp({cwd: pkgPath});
+  return {
+    json: pkgInfo.packageJson,
+    rootDir: path.dirname(pkgInfo.path)
+  };
+}
 
-  if (!pkgData) {
-    throw new Error('[resolveBin] Unable to find a package.json for the local project.');
+
+/**
+ * Resolves the absolute path to the binary of a given package. Some packages
+ * export multiple binaries, or binaries that do not match the package's name.
+ * In such cases, a second argument may be provided to indicate the name of the
+ * binary to look up.
+ */
+export function resolveBin(pkgName: string, binName?: string) {
+  // Resolve the absolute path to the desired package, starting from our current
+  // directory, thereby ensuring that any nested node_modules folders between
+  // us and the host package are included in the search.
+  const rootDir = resolvePkg(pkgName, { cwd: __dirname });
+
+  if (!rootDir) {
+    throw new Error(`${log.prefix('resolveBin')} Unable to resolve path to package "${pkgName}".`);
   }
 
-  if (!pkgData.packageJson.bin) {
-    throw new Error(`[resolveBin] Package "${pkgName}" does not declare any binaries.`);
+  // Load the package.json for the package.
+  const pkg = getPackageInfo(rootDir);
+
+  if (!pkg) {
+    throw new Error(`${log.prefix('resolveBin')} Unable to find a package.json for package "${pkgName}".`);
   }
 
-  // Using the path to the package's package.json, compute the root directory
-  // for the package.
-  const pkgRoot = path.parse(pkgData.path).dir;
+  if (!pkg.json.bin) {
+    throw new Error(`${log.prefix('resolveBin')} Package "${pkgName}" does not export any binaries.`);
+  }
 
-  // Extract the relative path to the indicated binary.
-  const relativeBinPath = binName ? pkgData.packageJson.bin[binName] : pkgData.packageJson.bin[pkgName];
+  // Get the relative path to the indicated binary.
+  const relativeBinPath = binName ? pkg.json.bin[binName] : pkg.json.bin[pkgName];
 
   if (!relativeBinPath) {
-    throw new Error(`[resolveBin] Package "${pkgName}" does not have binary "${binName ?? pkgName}".`);
+    throw new Error(`${log.prefix('resolveBin')} Package "${pkgName}" does not export a binary named "${binName ?? pkgName}".`);
   }
 
   // Return the absolute path to the indicated binary.
   return {
-    path: path.resolve(pkgRoot, relativeBinPath),
-    version: pkgData.packageJson.version
+    path: path.resolve(rootDir, relativeBinPath),
+    version: pkg.json.version
   };
 }
 
@@ -53,42 +73,29 @@ export async function resolveBin(pkgName: string, binName?: string) {
 /**
  * Provided a package name and optional binary name, loads the binary.
  */
-export async function requireBin(pkgName: string, binName?: string) {
-  const binInfo = await resolveBin(pkgName, binName);
-  log.verbose('bin', `Using ${log.chalk.bold(`${binName ?? pkgName}`)} version ${log.chalk.green(binInfo.version)}.`);
+export function requireBin(pkgName: string, binName?: string) {
+  const binInfo = resolveBin(pkgName, binName);
+  log.verbose(log.prefix('bin'), `Using ${log.chalk.bold(`${binName ?? pkgName}`)} version ${log.chalk.green(binInfo.version)}.`);
   require(binInfo.path);
 }
 
 
 /**
- * Synchronously reads package.json from the host package and returns its
- * contents and path.
- */
-export function getPackageInfo() {
-  const pkgInfo = readPkgUp.sync();
-
-  if (!pkgInfo) {
-    throw new Error('Unable to find a package.json for the project.');
-  }
-
-  return pkgInfo;
-}
-
-
-/**
+ * Used by our ESLint configuration.
+ *
  * Returns the path to the host package's tsconfig.json file. If a tsconfig.json
  * cannot be found, returns `false` and issues a warning telling the user they
  * will need to manually indicate the path to their tsconfig.json.
  */
 export function findTsConfig() {
-  const pkgRoot = path.dirname(getPackageInfo().path);
-  const tsConfigPath = path.resolve(pkgRoot, 'tsconfig.json');
+  const pkg = getPackageInfo();
+  const tsConfigPath = path.resolve(pkg.rootDir, 'tsconfig.json');
 
   try {
     fs.accessSync(tsConfigPath, fs.constants.R_OK);
   } catch (err) {
     if (err.code === 'ENOENT') {
-      log.warn(log.prefix('ts-unified'), [
+      log.warn(log.prefix('ts'), [
         'Attempted to automatically set ESLint\'s parserOptions.project to',
         `${log.chalk.green(tsConfigPath)}, but the file does not exist. You may need to set`,
         'parserOptions.project manually.'
@@ -103,44 +110,45 @@ export function findTsConfig() {
 
 
 /**
- * Because ts-unified shares the below scripts with its own dependents, we need
- * to differentiate when a binary is being invoked by ts-unified and when it is
- * being invoked by a dependent, because during the install/prepare phase for
- * ts-unified, NPM will not have linked the "bin" entries from our package.json
- * yet, and those entries point to files in our "dist" folder, which has not
- * been created yet.
+ * Because 'ts' shares the below scripts with its own dependents, we need
+ * to differentiate when a binary is being invoked by us and when it is being
+ * invoked by a dependent, because during out install/prepare phase, NPM will
+ * not have linked the "bin" entries from our package.json yet, and those
+ * entries point to files in our "dist" folder, which will not have been created
+ * yet.
  *
- * So, when used locally, a script needs to use the "raw" bin name, and when
- * used by a dependent, a script needs to use the "unified." prefix.
+ * So, when used by us, a script needs to use the canonical/standard bin name,
+ * and when used by a dependent, a script needs to use the prefixed bin name.
  *
  * This function checks the name of the closest package.json file (walking up
  * the directory tree from process.cwd) and compares it to our package name to
  * determine if prefixing should occur.
  */
 export function prefixBin(binName: string) {
-  const pkgInfo = getPackageInfo();
+  const pkg = getPackageInfo();
 
-  if (pkgInfo && pkgInfo.packageJson.name === '@darkobits/ts') {
+  if (pkg && pkg.json.name === '@darkobits/ts') {
     return binName;
   }
 
-  return `unified.${binName}`;
+  return `ts.${binName}`;
 }
 
 
 /**
- * Introspects the argument passed to this module's default export/function, and
- * returns an object representing any user-provided scripts/options.
+ * Introspects the argument passed to our NPS configuration function. If a
+ * configuration factory was provided, invokes it and returns the result. If an
+ * object was provided, returns it. If no argument was provided, returns a
+ * configuration scaffold object.
  */
-export function getUserScripts(userArgument: any) {
+export function getUserScripts(userArgument?: any) {
   let userScripts = {
     scripts: {},
     options: {}
   };
 
   if (typeof userArgument === 'function') {
-    // TODO: Determine if "bin" is still needed here.
-    userScripts = userArgument({ npsUtils, bin: prefixBin });
+    userScripts = userArgument({ npsUtils });
   }
 
   if (typeof userArgument === 'object') {
