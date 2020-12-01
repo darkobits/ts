60a641a4607508be05f5359d3daa2c97e568e5ba
darkobits <joshua+github@frontlawn.net>
2020-11-30 22:21:07 -0800

    misc: Update ESLint config.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,3 @@
 module.exports = {
-  extends: require('@darkobits/ts').eslint,
-  rules: {
-    // N.B. This package makes extensive use of require().
-    '@typescript-eslint/no-var-requires': 'off'
-  }
+  extends: require('@darkobits/ts').eslint
 };
