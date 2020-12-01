0ae9b4e3656eb7842ea00edb42b5356be7d1ec23
darkobits <joshua+github@frontlawn.net>
2020-12-01 00:45:06 -0800

    refactor: Hoist TypeScript base configuration to package root.

---
+++ re-pack.config.js
@@ -0,0 +1,10 @@
+const path = require('path');
+
+module.exports = {
+  afterRepack: ({ fs, packDir }) => {
+    fs.moveSync(
+      path.resolve(packDir, 'config', 'tsconfig-base.json'),
+      path.resolve(packDir, 'tsconfig.json')
+    );
+  }
+};
