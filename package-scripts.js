5ae6de65d2ac17a6346dd598bf9acb412571376b
darkobits <joshua+github@frontlawn.net>
2021-01-19 18:43:25 -0800

    feat: Add update notifier.

--- package-scripts.js
+++ package-scripts.js
@@ -2,6 +2,17 @@ require('./src/etc/babel-register');
 
 module.exports = require('./src').nps(() => ({
   scripts: {
+    prepareLocal: {
+      script: [
+        'nps build',
+        'nps test.passWithNoTests',
+        // N.B. Dependent packages will be able to use the ts.scripts.link-bins
+        // binary invoked in nps.ts, but because we are bootstrapping, we have
+        // to invoke this script in a slightly more verbose manner.
+        `node --require ${require.resolve('./src/etc/babel-register')} ${require.resolve('./src/bin/scripts/link-bins')}`,
+        'npx ts.scripts.update-notifier'
+      ].join(' && ')
+    },
     publish: {
       description: 'Re-pack and publish the package.',
       script: 're-pack publish'
