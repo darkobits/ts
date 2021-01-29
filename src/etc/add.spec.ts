bdd0e17a71c8165c069ab73215e49d3be538021c
darkobits <joshua+github@frontlawn.net>
2021-01-29 06:32:56 -0800

    chore: Update CI configuration.

--- src/etc/add.spec.ts
+++ src/etc/add.spec.ts
@@ -1,9 +1,6 @@
-import log from 'lib/log';
-
 // Here to make sure our Jest configuration hasn't been inadvertently broken.
 describe('add', () => {
   it('should add two numbers', () => {
-    log.info('foo');
     expect(2 + 2).toEqual(4);
   });
 });
