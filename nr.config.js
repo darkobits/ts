f125cce22308c4a0cde84ddd4ada344167c0e11a
darkobits <joshua+github@frontlawn.net>
2021-07-16 20:36:34 -0700

    misc: Add publish script.

--- nr.config.js
+++ nr.config.js
@@ -2,5 +2,15 @@ require('./src/etc/babel-register');
 
 // eslint-disable-next-line no-unused-vars
 module.exports = require('./src').nr(({ createCommand, createScript }) => {
-  // console.log('woo');
+  createScript({
+    name: 'publish',
+    group: 'Release',
+    description: 'Publish the package using re-pack.',
+    commands: [
+      createCommand({
+        command: 're-pack',
+        arguments: { _: ['publish'] }
+      })
+    ]
+  });
 });
