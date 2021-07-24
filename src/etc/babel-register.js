c759e7eb18f34c34633689dcff6f5ac094075c50
darkobits <joshua+github@frontlawn.net>
2021-07-24 04:29:17 -0700

    misc: Update CI workflow.

--- src/etc/babel-register.js
+++ src/etc/babel-register.js
@@ -1,6 +1,7 @@
 /**
  * Note: Do not import/require anything in this file and use CommonJS syntax
- * only. This file configures Babel, which allows the use of these features.
+ * only. This file configures Babel, which allows the use of other language
+ * features.
  */
 require('@babel/register')({
   extensions: ['.ts', '.tsx', '.js', '.jsx', '.mjs', '.cjs', '.json']
