bc9ff57dfd6dc512a4493bb7a90566a938fac60b
Joshua <joshua+github@frontlawn.net>
2021-08-14 19:23:27 -0700

    feat: Add 'node' preset.

--- src/config/babel.js
+++ src/config/babel.js
@@ -1,23 +1,32 @@
-// -----------------------------------------------------------------------------
-// ----- Babel Configuration ---------------------------------------------------
-// -----------------------------------------------------------------------------
 
-module.exports = {
-  extends: require('@darkobits/ts').babel,
-  presets: [
-    [require.resolve('@babel/preset-env'), {
-      useBuiltIns: 'entry',
-      corejs: 3
-    }],
-    require.resolve('@babel/preset-react'),
-    require.resolve('@linaria/babel-preset')
-  ],
-  plugins: [
-    require.resolve('@babel/plugin-transform-runtime'),
-    require.resolve('react-hot-loader/babel')
-  ],
-  // This is set to `true`, inverting the default setting from 'ts') to prevent
-  // Babel from stripping-out Webpack 'magic' comments before Webpack can parse
-  // them. Comments will then be removed by Webpack's minifier.
-  comments: true
+module.exports = api => {
+  const target = api.caller(caller => caller && caller.target);
+
+  // If compiling with Webpack and the 'target' has been set to 'node', use the
+  // base preset only.
+  if (target === 'node') {
+    return {
+      extends: require('@darkobits/ts').babel
+    };
+  }
+
+  return {
+    extends: require('@darkobits/ts').babel,
+    presets: [
+      [require.resolve('@babel/preset-env'), {
+        useBuiltIns: 'entry',
+        corejs: 3
+      }],
+      require.resolve('@babel/preset-react'),
+      require.resolve('@linaria/babel-preset')
+    ],
+    plugins: [
+      require.resolve('@babel/plugin-transform-runtime'),
+      require.resolve('react-hot-loader/babel')
+    ],
+    // This is set to `true`, inverting the default setting from 'ts') to
+    // prevent Babel from stripping-out Webpack 'magic' comments before Webpack
+    // can parse them. Comments will then be removed by Webpack's minifier.
+    comments: true
+  };
 };
--- src/config/webpack/index.ts
+++ src/config/webpack/index.ts
@@ -1,3 +1,4 @@
+import node from './node';
 import webpack from './react';
 import serverless from './serverless';
 import vanilla from './vanilla';
@@ -5,8 +6,6 @@ import vanilla from './vanilla';
 
 export default Object.assign(webpack, {
   serverless,
-  vanilla
-}) as typeof webpack & {
-  serverless: typeof serverless;
-  vanilla: typeof vanilla;
-};
+  vanilla,
+  node
+});
