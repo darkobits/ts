867ec506c8698aba8260808f305697b632d96853
Joshua <joshua+github@frontlawn.net>
2021-12-31 00:42:22 -0800

    chore: Update tests.

--- src/lib/inject-script.spec.ts
+++ src/lib/inject-script.spec.ts
@@ -1,36 +1,23 @@
+/**
+ * @jest-environment jsdom
+ */
 import { injectScript } from './inject-script';
 
-const headMock: Array<any> = [];
-
-const documentMock = {
-  head: {
-    append: (value: any) => headMock.push(value)
-  },
-  createElement: jest.fn(() => {
-    const el = {
-      addEventListener: jest.fn((name: string, cb: any) => {
-        if (name === 'load' && typeof cb === 'function') {
-          cb();
-        }
-      })
-    };
-
-    return el;
-  })
-};
 
 describe('injectScript', () => {
-  beforeEach(() => {
-    // @ts-expect-error
-    global.document = documentMock;
-  });
-
   it('should add a script tag to the document', async () => {
-    const src = 'src';
-    await injectScript(src);
-    const script = headMock[0];
-    expect(documentMock.createElement).toHaveBeenCalled();
-    expect(script.addEventListener).toHaveBeenCalledTimes(2);
-    expect(script.src).toBe(src);
+    const src = 'https://src.foo/';
+
+    const promise = injectScript(src);
+
+    const scriptTag = document.head.querySelector('script');
+    if (!scriptTag) throw new Error('No <script> tag found.');
+
+    scriptTag.dispatchEvent(new Event('load'));
+
+    await promise;
+
+    expect(scriptTag.src).toBe(src);
+    expect(scriptTag.async).toBe(true);
   });
 });
