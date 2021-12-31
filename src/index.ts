93c9054e5065dcb7d465cf8e59c9240adf11bd65
Joshua <joshua+github@frontlawn.net>
2021-12-31 13:56:18 -0800

    fix: Restore `jest` export.

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+export { default as jest } from '@darkobits/ts/config/jest';
 export { default as nr } from 'config/nr';
 export {default as vite } from 'config/vite';
 export const babel = require.resolve('config/babel');
