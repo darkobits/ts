81d9baa9f32413d7c72981463d01c0b4476c2f83
Joshua <joshua+github@frontlawn.net>
2022-01-17 21:35:00 -0800

    fix: Update Babel config.

--- src/config/babel.js
+++ src/config/babel.js
@@ -1,30 +1,28 @@
-module.exports = () => {
-  return {
-    extends: require('@darkobits/ts').babel,
-    presets: [
-      ['@babel/preset-env', {
-        useBuiltIns: 'entry',
-        corejs: 3,
-        // Do not transpile import() statements. This will allow packages that
-        // publish CommonJS to import ES Modules.
-        exclude: ['@babel/plugin-proposal-dynamic-import']
-      }],
-      '@babel/preset-typescript',
-      '@babel/preset-react',
-      '@linaria/babel-preset'
-    ],
-    plugins: [
-      'babel-plugin-transform-import-meta',
-      '@babel/plugin-transform-runtime',
-      ['@babel/plugin-proposal-decorators', { legacy: true, loose: true }],
-      // At the moment, this is required for Linaria to work.
-      ['babel-plugin-module-resolver', {
-        cwd: 'packagejson',
-        root: ['./src'],
-        extensions: ['.ts', '.tsx', '.js', '.jsx', '.cjs', '.mjs', '.json']
-      }]
-    ],
-    comments: false,
-    sourceType: 'unambiguous'
-  };
+module.exports = {
+  extends: require('@darkobits/ts').babel,
+  presets: [
+    ['@babel/preset-env', {
+      useBuiltIns: 'entry',
+      corejs: 3,
+      // Do not transpile import() statements. This will allow packages that
+      // publish CommonJS to import ES Modules.
+      exclude: ['@babel/plugin-proposal-dynamic-import']
+    }],
+    '@babel/preset-typescript',
+    '@babel/preset-react',
+    '@linaria/babel-preset'
+  ],
+  plugins: [
+    'babel-plugin-transform-import-meta',
+    '@babel/plugin-transform-runtime',
+    ['@babel/plugin-proposal-decorators', { legacy: true, loose: true }],
+    // At the moment, this is required for Linaria to work.
+    ['babel-plugin-module-resolver', {
+      cwd: 'packagejson',
+      root: ['./src'],
+      extensions: ['.ts', '.tsx', '.js', '.jsx', '.cjs', '.mjs', '.json']
+    }]
+  ],
+  comments: false,
+  sourceType: 'unambiguous'
 };
