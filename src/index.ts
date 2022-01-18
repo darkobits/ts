33d76f43d41b76317e83da065e2e4e39586b6cbc
Joshua <joshua+github@frontlawn.net>
2022-01-17 21:31:43 -0800

    chore(deps): Update dependencies.

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
-export { default as jest } from '@darkobits/ts/config/jest';
+export { jest } from '@darkobits/ts';
 export { default as nr } from 'config/nr';
-export {default as vite } from 'config/vite';
+export { default as vite } from 'config/vite';
 export const babel = require.resolve('config/babel');
 export const eslint = require.resolve('config/eslint');
