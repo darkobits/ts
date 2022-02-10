7d98bc5e93fb5263d0e8fb0180dee229d6dbf60a
Joshua <joshua+github@frontlawn.net>
2022-02-10 09:45:14 -0800

    fix: Update Babel ESM resolver.

--- src/config/babel-esm.js
+++ src/config/babel-esm.js
@@ -1,6 +1,10 @@
+const Module = require('module');
 const path = require('path');
 
-const { resolvePath } = require('babel-plugin-module-resolver');
+const { resolvePath: pluginModuleResolver } = require('babel-plugin-module-resolver');
+const readPkgUp = require('read-pkg-up');
+const resolveFrom = require('resolve-from');
+const resolvePkg = require('resolve-pkg');
 
 const { EXTENSIONS_WITH_DOT } = require('../etc/constants');
 const cjsConfig = require('./babel');
@@ -8,40 +12,85 @@ const cjsConfig = require('./babel');
 const { TS_ENV } = process.env;
 
 
-/**
- * Because extensions are required in import identifiers in ES Modules (except
- * when importing a bare package name), we need to add them to identifiers that
- * import other files in the local package or files within other packages.
- */
-function customResolvePath(sourcePath, currentFile, opts) {
-  // Do not attempt to resolve internal, data, and file URL schemes.
-  // See: https://nodejs.org/api/esm.html#urls
-  if (sourcePath.startsWith('node:') || sourcePath.startsWith('data:') || sourcePath.startsWith('file:')) {
-    return sourcePath;
+function resolvePackage(sourcePath) {
+  try {
+    return require.resolve(sourcePath);
+  } catch {
+    return resolvePkg(sourcePath);
   }
-
-  const resolvedPath = resolvePath(sourcePath, currentFile, opts);
-
-  // Handle import identifiers for node_modules packages and files
-  // therein.
-  if (resolvedPath === null) {
-    return sourcePath;
-  }
-
-  // This case will handle all files internal to the project.
-  if (typeof resolvedPath === 'string') {
-    // Add extensions to imports without them.
-    if (path.extname(resolvedPath) === '') {
-      return `${resolvedPath}.js`;
-    }
-
-    // Change extensions to .js for imports with extensions.
-    return resolvedPath.replace(/\.\w{2}$/g, '.js');
-  }
-
-  // eslint-disable-next-line unicorn/no-null
-  return null;
 }
+
+
+function introspectPackageImport(sourcePath, fromDir) {
+  const resolvedPkg = resolvePackage(sourcePath);
+  if (!resolvedPkg) return;
+
+  const absoluteFilePath = resolveFrom(fromDir, sourcePath);
+  const pkgInfo = readPkgUp.sync({ cwd: path.dirname(absoluteFilePath) });
+  const kind = sourcePath === pkgInfo.packageJson.name ? 'bare' : 'file';
+  const specifier = kind === 'file'
+    ? absoluteFilePath.slice(absoluteFilePath.indexOf(sourcePath))
+    : sourcePath;
+
+  return {
+    pkgName: pkgInfo.packageJson.name,
+    pkgRoot: path.dirname(pkgInfo.path),
+    kind,
+    specifier
+  };
+}
+
+
+function changeExtension(sourcePath, ext) {
+  const parsed = path.parse(sourcePath);
+  parsed.base = !parsed.ext ? `${parsed.base}${ext}` : parsed.base.replace(/\.\w{2,}$/g, ext);
+  parsed.ext = ext;
+  return path.format(parsed);
+}
+
+
+function customResolvePath(sourcePath, currentFile, opts) {
+  const fromDir = path.dirname(currentFile);
+
+  // ----- Node Built-Ins ------------------------------------------------------
+
+  if (Module.builtinModules.includes(sourcePath)) {
+    return sourcePath;
+  }
+
+  if (sourcePath.startsWith('node:') && Module.builtinModules.includes(sourcePath.slice(5))) {
+    return sourcePath;
+  }
+
+
+  // ----- URL Schemes ---------------------------------------------------------
+
+  if ( sourcePath.startsWith('data:') || sourcePath.startsWith('file:')) {
+    return sourcePath;
+  }
+
+
+  // ----- Internal Files ------------------------------------------------------
+
+  // Note: This will be `null` if importing from outside the package.
+  const resolvedInternalPath = pluginModuleResolver(sourcePath, currentFile, opts);
+
+  if (resolvedInternalPath) {
+    return changeExtension(resolvedInternalPath, '.js');
+  }
+
+
+  // ----- External Packages / Files -------------------------------------------
+
+  const result = introspectPackageImport(sourcePath, fromDir);
+
+  if (!result) {
+    throw new Error(`Unable to resolve specifier: ${sourcePath}`);
+  }
+
+  return result.specifier;
+}
+
 
 module.exports = TS_ENV === 'esm' ? {
   presets: [
