219539ccf4786a865cd6a324624313af9acb1201
Joshua <joshua+github@frontlawn.net>
2023-01-25 02:47:50 -0800

    refactor: Move `tsconfig.json` to `config` folder.

--- re-pack.config.ts
+++ re-pack.config.ts
@@ -2,12 +2,15 @@ import path from 'path';
 
 export default {
   afterRepack: ({ fs, packDir }) => {
-    // Moves dist/config/tsconfig-base.json to tsconfig.json in the re-packed
-    // package root. This allows consumers to "extend": "@darkobits/ts" in their
-    // tsconfig.json files.
-    fs.copySync(
+    // Moves config/tsconfig-base.json to config/tsconfig.json in the re-packed
+    // package root. This allows consumers to extend
+    // "@darkobits/ts/config/tsconfig.json" in their tsconfig files.
+    // Note: Moving this to the package root to allow extending "@darkobits/ts"
+    // no longer works because Vitest will try to load index.js instead of
+    // tsconfig.js.
+    fs.moveSync(
       path.resolve(packDir, 'config', 'tsconfig-base.json'),
-      path.resolve(packDir, 'tsconfig.json')
+      path.resolve(packDir, 'config', 'tsconfig.json')
     );
   }
 };
