cfc205c80be6fe802cb4752337c5ec784db8db10
Joshua <joshua+github@frontlawn.net>
2023-01-25 10:00:25 -0800

    fix(tsconfig): Relocate `tsconfig.json` to package root.

--- re-pack.config.ts
+++ re-pack.config.ts
@@ -2,15 +2,13 @@ import path from 'path';
 
 export default {
   afterRepack: ({ fs, packDir }) => {
-    // Moves config/tsconfig-base.json to config/tsconfig.json in the re-packed
-    // package root. This allows consumers to extend
-    // "@darkobits/ts/config/tsconfig.json" in their tsconfig files.
-    // Note: Moving this to the package root to allow extending "@darkobits/ts"
-    // no longer works because Vitest will try to load index.js instead of
-    // tsconfig.js.
+    // Moves config/tsconfig-base.json to tsconfig.json in the re-packed package
+    // root. This allows consumers to extend "@darkobits/ts/tsconfig.json" in
+    // their tsconfig files. Note: Users cannot use a simple package identifier
+    // like "@darkobits/ts" because Vitest/Vite will try to load 'index.js'.
     fs.moveSync(
       path.resolve(packDir, 'config', 'tsconfig-base.json'),
-      path.resolve(packDir, 'config', 'tsconfig.json')
+      path.resolve(packDir, 'tsconfig.json')
     );
   }
 };
