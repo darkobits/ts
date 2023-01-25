cc8b350cfad62745737224eb3ef4e266a0d9a103
Joshua <joshua+github@frontlawn.net>
2023-01-25 14:15:11 -0800

    refactor: Add exports for extensions and utility functions.

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,6 @@
 export { default as nr } from './config/nr';
 export { default as vitest } from './config/vitest';
+
+export { EXTENSIONS } from './etc/constants';
+
+export { getSourceAndOutputDirectories } from './lib/utils';
