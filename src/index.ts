36bc6f26d20bdeeb492958b0de0957393a4911c3
Joshua <joshua+github@frontlawn.net>
2023-02-10 23:38:06 -0800

    fix: Improve interop with ESM consumers.

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,14 @@
-export { default as nr } from './config/nr';
-export { default as vitest } from './config/vitest';
+/* eslint-disable unicorn/prefer-export-from */
+import nr from './config/nr';
+import vitest from './config/vitest';
+import { EXTENSIONS } from './etc/constants';
+import { getSourceAndOutputDirectories } from './lib/utils';
 
-export { EXTENSIONS } from './etc/constants';
-
-export { getSourceAndOutputDirectories } from './lib/utils';
+// This approach, rather than "export from" syntax, ensures TypeScript compiles
+// our code such that named imports in ESM consumers work as expected.
+export {
+  nr,
+  vitest,
+  getSourceAndOutputDirectories,
+  EXTENSIONS
+};
