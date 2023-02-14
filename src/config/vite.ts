ae2c94be1ec3a8e39c7d1644e1ae3043353d4382
Joshua <joshua+github@frontlawn.net>
2023-02-13 21:56:24 -0800

    fix: Fix plugin default imports.

--- src/config/vite.ts
+++ src/config/vite.ts
@@ -3,7 +3,7 @@ import typescriptPlugin from '@rollup/plugin-typescript';
 import glob from 'fast-glob';
 import eslintPluginExport from 'vite-plugin-eslint';
 // @ts-expect-error - Package has no type definitions.
-import noBundlePlugin from 'vite-plugin-no-bundle';
+import noBundlePluginExport from 'vite-plugin-no-bundle';
 import tsconfigPathsPluginExport from 'vite-tsconfig-paths';
 
 
@@ -18,14 +18,25 @@ import {
 // Fix default imports for problematic packages.
 const tsconfigPathsPlugin = interopRequireDefault(tsconfigPathsPluginExport, 'vite-tsconfig-paths');
 const eslintPlugin = interopRequireDefault(eslintPluginExport, 'vite-plugin-eslint');
+const noBundlePlugin = interopRequireDefault(noBundlePluginExport, 'vite-plugin-no-bundle');
 
 
+/**
+ * Vite configuration preset suitable for publishing libraries to NPM.
+ *
+ * - Source files will not be bundled.
+ * - `node_modules` will be externalized
+ * - Output format (CJS or ESM) will be inferred from the `type` field in
+ *   `package.json`.
+ * - Source and output directories will be inferred from `tsconfig.json`.
+ */
 export const library = createViteConfigurationPreset(async context => {
   // Compute entries.
   const entry = await glob(`${context.srcDir}/**/*.{${BARE_EXTENSIONS.join(',')}}`, {
     cwd: context.root,
     ignore: [`**/*.{${TEST_FILE_PATTERNS.join(',')}}.*`]
   });
+
   if (entry.length === 0) throw new Error(`[vite-config] No suitable entries found in ${context.srcDir}`);
 
   // Global source map setting used by various plug-ins below.
