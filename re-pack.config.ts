695192f111dfe9ca530a96be51d3f831daf0568f
Joshua <joshua+github@frontlawn.net>
2023-02-15 17:55:29 -0800

    chore: Misc. cleanup.

--- re-pack.config.ts
+++ re-pack.config.ts
@@ -1,6 +1,8 @@
 import path from 'path';
 
 export default {
+  // @ts-expect-error - We have no way of knowing the signature of this
+  // function. TODO: Add helper `defineConfig` in re-pack.
   afterRepack: ({ fs, packDir }) => {
     // Moves config/tsconfig-base.json to tsconfig.json in the re-packed package
     // root. This allows consumers to extend "@darkobits/ts/tsconfig.json" in
