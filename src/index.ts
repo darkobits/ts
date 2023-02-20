c6ffa550baed3aee94ab43bf2754cbcc3a6a3982
Joshua <joshua+github@frontlawn.net>
2023-02-20 08:05:18 -0800

    refactor: Update exports.

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,2 @@
-/* eslint-disable unicorn/prefer-export-from */
-
-import nr from './config/nr';
-import * as vite from './config/vite';
-import { EXTENSIONS } from './etc/constants';
-import { getPackageContext } from './lib/utils';
-
-// This approach, rather than "export from" syntax, ensures TypeScript compiles
-// our code such that named imports in ESM consumers work as expected.
-export {
-  nr,
-  vite,
-  getPackageContext,
-  EXTENSIONS
-};
+export { default as nr } from './config/nr';
+export * as vite from './config/vite';
