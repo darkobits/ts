b3b623d1656f648dca8ff84dfc909ef8ef5c512b
Joshua <joshua+github@frontlawn.net>
2023-02-23 02:09:01 -0800

    chore(standard-version): Release commits use skip CI token.

--- src/config/changelog-preset.js
+++ src/config/changelog-preset.js
@@ -3,8 +3,11 @@ const config = require('conventional-changelog-conventionalcommits');
 
 /**
  * Custom CHANGELOG preset based on conventional-changelog.
+ *
+ * See: https://github.com/conventional-changelog/conventional-changelog-config-spec
  */
 module.exports = config({
+  releaseCommitMessageFormat: 'chore(release): {{currentTag}}\n[skip ci]',
   types: [
     {type: 'feat', section: '✨ Features'},
     {type: 'fix', section: '🐞 Bug Fixes'},
