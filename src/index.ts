192b7ef8af0ba9e0de8c4dd6761f5797c10b5a6c
Joshua <joshua+github@frontlawn.net>
2023-07-03 03:25:35 -0700

    chore: Add `withDefaultPackageScripts` export.

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,6 @@
+export { default as withDefaultPackageScripts } from './config/nr';
+// TODO: Deprecate this.
 export { default as nr } from './config/nr';
+
 export * as vite from './config/vite';
 export { gitDescribe } from './lib/utils';
